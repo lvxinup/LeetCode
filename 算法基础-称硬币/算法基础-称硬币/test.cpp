f0796a26715577895ecbd3d9fec4499684591284
lvxinup <1564605677@qq.com>
2019-04-20 16:13:39 +0800

    算法学习

---
+++ 算法基础-称硬币/算法基础-称硬币/test.cpp
@@ -0,0 +1,23 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+//有12枚硬币。其中有11枚真币和1枚假币。假币和真币重量不同，但不知道假币比真币轻还是重。
+//现在，用一架天平称了这些币三次，告诉你称的结果，请你找出假币并且确定假币是轻是重(数据保证一定能找出来)
+//
+//输入样例
+//1
+//ABCD EFGH evenABCI EFJK upABIJ EFGH even
+//输出样例
+//K is the counterfeit coin and it is light.
+
+#include<iostream>
+#include<stdio.h>
+#include<vector>
+using namespace std;
+
+int main()
+{
+	vector < vector<int>> v;
+	vector<int> tmp;
+
+	return 0;
+}
