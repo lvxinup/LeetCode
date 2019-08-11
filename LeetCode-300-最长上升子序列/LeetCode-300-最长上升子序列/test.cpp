fdeea9ec5700eb1752dd3ef879b541d4c348514b
lvxinup <1564605677@qq.com>
2019-08-11 09:16:48 +0800

    LeetCode-300-最长上升子序列

---
+++ LeetCode-300-最长上升子序列/LeetCode-300-最长上升子序列/test.cpp
@@ -0,0 +1,18 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+class Solution {
+public:
+	int lengthOfLIS(vector<int>& nums) {
+		vector<int> dp(nums.size(), 1);
+		int res = 0;
+		for (int i = 0; i < nums.size(); ++i) {
+			for (int j = 0; j < i; ++j) {
+				if (nums[i] > nums[j]) {
+					dp[i] = max(dp[i], dp[j] + 1);
+				}
+			}
+			res = max(res, dp[i]);
+		}
+		return res;
+	}
+};
